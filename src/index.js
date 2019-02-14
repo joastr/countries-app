94775f305a8706815909c5d977be462049dac22b
Joanna Kasperek <joastr1@gmail.com>
2019-02-14 20:47:27 +0100

    add routers - task part2

--- src/index.js
+++ src/index.js
@@ -2,15 +2,19 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './store';
-import DevTools from './DevTools';
+//import DevTools from './DevTools';
+import { Router, hashHistory } from 'react-router';
+import routes from './routes';
+import './country.css';
 
 
 
-
-render (
-    <Provider store ={store}>
-        <h1>Inicjalizacja projektu</h1>
-        <DevTools/>
+render(
+    <Provider store={store}>
+        <div>    
+            <Router history={hashHistory} routes={routes}/>
+            
+       </div> 
     </Provider>,
     document.getElementById('root')
 );
